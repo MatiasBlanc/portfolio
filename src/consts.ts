a573c1e47390be7fe03fc8db0fafbb26a5a96acf
Matias Blanc <matias.f.blanc@gmail.com>
2026-07-20 02:41:22 -0400

    init: setup astro framework and base configuration

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Matias Blanc';
+export const SITE_DESCRIPTION = 'Desarrollo de soluciones de software escalables, aplicaciones web modernas y automatización técnica. soy un studiante de Ingeniería Civil en Computación e Informática en la UCN.';
